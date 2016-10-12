3f245b62e86c5f4037fed8650253ca00b225cc32
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-10-12 21:46:54 +0200

    use hdf5 for storing processed data

--- reduce.py
+++ reduce.py
@@ -1,12 +1,20 @@
 from modules import file_IO, preprocessing
 import numpy as np
+import h5py
+
+f = h5py.File("preprocessed/reduced.hdf5", "w")
 
 def reduce(out_name, dir):
     reduced = preprocessing.remove_zero_columns(file_IO.load_directory(dir))
-    np.savetxt("preprocessed/"+out_name, reduced)
+    f.create_dataset(out_name, data=reduced)
+
     print("saved: ", out_name, "with size ",reduced.shape)
 
+
+
 for name in ["train", "test"]:
-    reduce(name+"_grey_reduced.csv", "set_"+name+"/grey_matter/")
-    reduce(name+"_white_reduced.csv", "set_"+name+"/white_matter/")
-    reduce(name+"_fluid_reduced.csv", "set_"+name+"/spinal_fluid/")
+    reduce(name+"_grey", "set_"+name+"/grey_matter/")
+    reduce(name+"_white", "set_"+name+"/white_matter/")
+    reduce(name+"_fluid", "set_"+name+"/spinal_fluid/")
+
+f.close()
