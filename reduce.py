214116855aaf6fab99729cfd5ff7fb2d8fa6702b
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-10-14 10:49:49 +0200

    remove columns of train and test dataset together

--- reduce.py
+++ reduce.py
@@ -7,18 +7,27 @@ block_dims = np.array([4,4,4])
 
 f = h5py.File("preprocessed/reduced.hdf5", "w")
 
-for name in ["train", "test"]:
-    data1 = preprocessing.remove_zero_columns(
-        file_IO.load_directory("set_" + name + "/grey_matter/", block_dims))
-    print("loaded grey with size", data1.shape)
-    data2 = preprocessing.remove_zero_columns(
-        file_IO.load_directory("set_" + name + "/white_matter/", block_dims))
-    print("loaded white with size", data2.shape)
-    data3 = preprocessing.remove_zero_columns(
-        file_IO.load_directory("set_" + name + "/spinal_fluid/", block_dims))
-    print("loaded fluid with size", data3.shape)
+def load_component(comp_name):
+    """
+    return: 1) array of shape n_train+n_test, comp_size
+            2) n_train
+    """
+    data = file_IO.load_directory("set_train/"+comp_name+"/", block_dims)
+    n_train = data.shape[0]
+    data = np.concatenate((data,
+                           file_IO.load_directory("set_test/" + comp_name + "/", block_dims)
+                          ), axis = 0)
+    return preprocessing.remove_zero_columns(data), n_train
 
-    f.create_dataset(name, data=np.concatenate((data1, data2, data3), axis=1))
-    print("saved: ", name)
+data1, n_train = load_component("grey_matter")
+print("loaded grey with size", data1.shape)
+data2, _ = load_component("white_matter")
+print("loaded white with size", data2.shape)
+data3, _ = load_component("spinal_fluid")
+print("loaded fluid with size", data3.shape)
+
+f.create_dataset("train_data", data=np.concatenate((data1, data2, data3), axis=1)[:n_train, :])
+f.create_dataset("test_data",  data=np.concatenate((data1, data2, data3), axis=1)[n_train:, :])
+print("n train dataset: ", n_train)
 
 f.close()
