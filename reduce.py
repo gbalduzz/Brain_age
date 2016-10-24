3e0d4fcf0d17eb9638c9ee328c5ed7f9f5222368
Giovanni <gbalduzz@itp.phys.ethz.ch>
2016-10-24 13:50:52 +0200

    Use all voxels

--- reduce.py
+++ reduce.py
@@ -2,8 +2,6 @@ from modules import file_IO, preprocessing
 import numpy as np
 import h5py
 
-# Size of the data block to be averaged.
-block_dims = np.array([2,2,2])
 
 f = h5py.File("preprocessed/reduced.hdf5", "w")
 
@@ -12,12 +10,12 @@ def load_component(comp_name):
     return: 1) array of shape n_train+n_test, comp_size
             2) n_train
     """
-    data = file_IO.load_directory("set_train/"+comp_name+"/", block_dims)
+    data = file_IO.load_directory("set_train/"+comp_name+"/")
     n_train = data.shape[0]
     data = np.concatenate((data,
-                           file_IO.load_directory("set_test/" + comp_name + "/", block_dims)
+                           file_IO.load_directory("set_test/" + comp_name + "/")
                           ), axis = 0)
-    return preprocessing.remove_zero_columns(data), n_train
+    return preprocessing.remove_zero_columns(data, n_train), n_train
 
 data1, n_train = load_component("grey_matter")
 print("loaded grey with size", data1.shape)
